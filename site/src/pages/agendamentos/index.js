780b88ad8b38163542a7aef154da27baf94a0ca7
LucaasAndrade <lucasmarcelodeandradesilva@gmail.com>
2022-06-18 14:11:23 -0300

    pagina consulta

--- site/src/pages/agendamentos/index.js
+++ site/src/pages/agendamentos/index.js
@@ -1,6 +1,12 @@
 
 import './index.scss'
 
+import logo from '../../assets/images/logo.png';
+import lupa from '../../assets/images/lupa.png';
+import lapis from '../../assets/images/ferramenta-lapis.png';
+import lixeira from '../../assets/images/lixeira.png';
+import calendario from '../../assets/images/calendario.png';
+
 export default function Index(){
 
 
@@ -8,7 +14,7 @@ export default function Index(){
         <main className='page-consultar'>
             <section className='cabecalho'>
                 <div className='img-cabecalho'>
-                    <img src='' />
+                    <img src={logo} />
                 </div>
                 <div className='titulo-cabecalho'>
                     <h3>
@@ -17,20 +23,42 @@ export default function Index(){
                 </div>
                 <div className='barraPesquisa-cabecalho'>
                     <input type='text' placeholder='Digite a data da consulta' />
-                    <img src='' />
+                    <img src={lupa} />
                 </div>
             </section>
             <section className='faixa-principal'>
                     <div className='card'>
                         <div className='titulo-card' >
                             <h4> Consulta </h4>
-
-                            <img src='' />
-                            <img src='' />
+                            <div className='img-cards'>
+                                <img src={lapis} />
+                                <img src={lixeira} />
+                            </div>
                         </div>
                         
                         <div className='data-card'>
-                            <img src='' />
+                            <img src={calendario} />
+
+                            <p> dd/mm/aaaa</p>
+                        </div>
+
+                        <div className='descricao-card'>
+                            <p> Maria de Loudes Rodrigues</p>
+                            <p> Dra. Hanna Lewis</p>
+                        </div>
+                    </div>
+                    
+                    <div className='card'>
+                        <div className='titulo-card' >
+                            <h4> Consulta </h4>
+                            <div className='img-cards'>
+                                <img src={lapis} />
+                                <img src={lixeira} />
+                            </div>
+                        </div>
+                        
+                        <div className='data-card'>
+                            <img src={calendario} />
 
                             <p> dd/mm/aaaa</p>
                         </div>
