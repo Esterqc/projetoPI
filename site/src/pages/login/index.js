b088d90234e6191ae6db9e3b0edadd1b8f4f8097
ester queiroz de carvalho <esterq292@gmail.com>
2022-06-20 01:06:07 -0300

    css login

--- site/src/pages/login/index.js
+++ site/src/pages/login/index.js
@@ -44,42 +44,49 @@ export default function Index() {
 
   }
 
-  return (
-    <main className='page-login'>
-      <LoadingBar color='#287D01' ref={ref} />
-
-      <section className='faixa-login'>
-        <div className="logo">
-          <img src="/images/odontotooths 1.svg" alt="logo" />
-          <a className="voltar" href="../home">Voltar</a>
-        </div>
-        <div className="destacar-login">
-          <div className="conteudo">
-           
-
-
-            <div className="txt-icones1">
-            <img src="/images/o-email 1.svg" alt="email" width='30'/>
-            <h2 class="subtitulo"> E-mail*</h2>
-           <input class="tag-input1" type="text" placeholder='ex: exemplo@gmail.com' value={email} onChange={e => setEmail(e.target.value)} />
-            </div>
-
-            <div className="txt-icones2">
-            <img src="/images/cadeado 1.svg" alt="cadeado" width='30' />
-              <h2 class="subtitulo">Senha*</h2>
-              <input class="tag-input2" type="password" placeholder='***' value={senha} onChange={e => setSenha(e.target.value)} />
-            </div>
-
-            
-            <button className="botao" onClick={entrarClick} disabled={carregando}>Entrar</button>
-            
-            <div className='erro'>
-              {erro}
-            </div>
+  
+    return (
+      <main className='page-login'>
+        <LoadingBar color='#287D01' ref={ref} />
+  
+        <section className='faixa-login'>
+          <div className="logo">
+            <img src="/images/odontotooths 1.svg" alt="logo" />
+            <a className="voltar" href="../home">Voltar</a>
           </div>
-
-        </div>
-      </section>
-    </main>
-  );
+          <div className="destacar-login">
+            <div className="conteudo">
+  
+              <div>
+                <div className='textos'>
+                  <img src="/images/o-email 1.svg" alt="email" width='25' />
+                  <h2 class="subtitulo"> E-mail</h2>
+                </div>
+                <div>
+                  <input class="tag-input" type="text" placeholder='ex: exemplo@gmail.com' value={email} onChange={e => setEmail(e.target.value)} />
+                </div>
+              </div>
+  
+              <div>
+                <div className='textos'>
+                  <img src="/images/cadeado 1.svg" alt="cadeado" width='25' />
+                  <h2 class="subtitulo">Senha</h2>
+                </div>
+                <div>
+                  <input class="tag-input" type="password" placeholder='***' value={senha} onChange={e => setSenha(e.target.value)} />
+                </div>
+              </div>
+  
+  
+              <button className="botao" onClick={entrarClick} disabled={carregando}>Entrar</button>
+  
+              <div className='erro'>
+                {erro}
+              </div>
+            </div>
+  
+          </div>
+        </section>
+      </main>
+    );
 }
